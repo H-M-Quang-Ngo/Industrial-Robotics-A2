7f25afb2b1e8f8803c8e197c81f616ebb3f2ebf6
H-M-Quang-Ngo <113411417+H-M-Quang-Ngo@users.noreply.github.com>
2022-09-21 18:53:51 +1000

    Color Robot

--- Dorna2Robot.m
+++ Dorna2Robot.m
@@ -26,32 +26,40 @@ classdef Dorna2Robot < handle
 
         function PlotAndColourRobot(self)
             for linkIndex = 0:self.model.n
-                [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread(['Dorna2Robot_J',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>                
-                self.model.faces{linkIndex + 1} = faceData;
-                self.model.points{linkIndex + 1} = vertexData;
+                if self.useGripper && linkIndex == self.model.n
+                    [ faceData, vertexData, plyData{linkIndex+1} ] = plyread([self.plyFileNameStem,'_J',num2str(linkIndex),'Gripper.ply'],'tri'); %#ok<AGROW>
+                else
+                    [ faceData, vertexData, plyData{linkIndex+1} ] = plyread([self.plyFileNameStem,'_J',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
+                end
+                self.model.faces{linkIndex+1} = faceData;
+                self.model.points{linkIndex+1} = vertexData;
             end
 
             % Display robot
             self.model.plot3d(zeros(1,self.model.n),'noarrow','workspace',self.workspace);
             if isempty(findobj(get(gca,'Children'),'Type','Light'))
                 camlight
-            end  
+            end
             self.model.delay = 0;
 
-             % Try to correctly colour the arm (if colours are in ply file data)
+            % Try to manually colour the arm
             for linkIndex = 0:self.model.n
                 handles = findobj('Tag', self.model.name);
                 h = get(handles,'UserData');
-                try 
-                    h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
-                                                                  , plyData{linkIndex+1}.vertex.green ...
-                                                                  , plyData{linkIndex+1}.vertex.blue]/255;
-                    h.link(linkIndex+1).Children.FaceColor = 'interp';
-                catch ME_1
-                    disp(ME_1);
-                    continue;
+                
+                switch (linkIndex + 1)
+                    case 1 
+                        vertexColours = [0,0.1,1];
+                    case 5
+                        vertexColours = [0,0,0.5];
+                    case 6
+                        vertexColours = [1, 1, 1];
+                    otherwise
+                        vertexColours = [0,0.4,1]; % Default colours
                 end
+                h.link(linkIndex+1).Children.FaceVertexCData = repmat(vertexColours,[numrows(plyData{linkIndex+1}.vertex.x) 1]);
+                h.link(linkIndex+1).Children.FaceColor = 'interp';
             end
-        end        
+        end
     end
 end
