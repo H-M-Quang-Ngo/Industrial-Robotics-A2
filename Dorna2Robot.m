eda01779492c836ec94f0e42f5a82c74fb59df1b
H-M-Quang-Ngo <113411417+H-M-Quang-Ngo@users.noreply.github.com>
2022-09-22 23:42:19 +1000

    small update

--- Dorna2Robot.m
+++ Dorna2Robot.m
@@ -1,18 +1,27 @@
 classdef Dorna2Robot < handle
     properties
+        %> Robot model
         model
-        useGripper = false;
+
+        %> Robot name
+        name = 'Dorna2Robot'
+
+        %> Workspace
         workspace = [-2 2 -2 2 -0.3 2];
-        plyFileNameStem = 'Dorna2Robot'
+
+        % default set of joints
         defaultJoint = [0 -80 100 -20 0]*pi/180;
     end
-    %% Define Robot Function
+
+
     methods
+        %% Define Robot Function
         function self = Dorna2Robot()
             self.CreateModel();
             self.PlotAndColourRobot();
             self.model.animate(self.defaultJoint)
         end
+
         %% Model Robot's DH Parameters
         function CreateModel(self)
             L(1) = Link('d',0.2064,   'a',0.09548, 'alpha',-pi/2, 'offset',0,     'qlim',[deg2rad(-160),deg2rad(180)]);
@@ -21,22 +30,23 @@ classdef Dorna2Robot < handle
             L(4) = Link('d',0,        'a',0,       'alpha',pi/2,  'offset',pi/2,  'qlim',[deg2rad(-135),deg2rad(135)]);
             L(5) = Link('d',0.04892,  'a',0,       'alpha',0,     'offset',0,     'qlim',[deg2rad(-360),deg2rad(360)]);
 
-            self.model = SerialLink(L,'name','Dorna 2');
+            self.model = SerialLink(L,'name',self.name);
         end
 
+        %% Plot and Colour Robot
         function PlotAndColourRobot(self)
             for linkIndex = 0:self.model.n
-                if self.useGripper && linkIndex == self.model.n
-                    [ faceData, vertexData, plyData{linkIndex+1} ] = plyread([self.plyFileNameStem,'_J',num2str(linkIndex),'Gripper.ply'],'tri'); %#ok<AGROW>
-                else
-                    [ faceData, vertexData, plyData{linkIndex+1} ] = plyread([self.plyFileNameStem,'_J',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
-                end
+                [faceData, vertexData, plyData{linkIndex+1} ] = plyread([self.name,'_J',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
                 self.model.faces{linkIndex+1} = faceData;
                 self.model.points{linkIndex+1} = vertexData;
             end
 
+            % Options to display robot
+            plotopts = {'fps',240,'workspace',self.workspace,...
+                'noarrow','nowrist','noname','tile1color',[1 1 1],'floorlevel',-0.3};
+
             % Display robot
-            self.model.plot3d(zeros(1,self.model.n),'noarrow','workspace',self.workspace);
+            self.model.plot3d(zeros(1,self.model.n),plotopts{:});
             if isempty(findobj(get(gca,'Children'),'Type','Light'))
                 camlight
             end
@@ -46,9 +56,9 @@ classdef Dorna2Robot < handle
             for linkIndex = 0:self.model.n
                 handles = findobj('Tag', self.model.name);
                 h = get(handles,'UserData');
-                
+
                 switch (linkIndex + 1)
-                    case 1 
+                    case 1
                         vertexColours = [0,0.1,1];
                     case 5
                         vertexColours = [0,0,0.5];
@@ -61,5 +71,22 @@ classdef Dorna2Robot < handle
                 h.link(linkIndex+1).Children.FaceColor = 'interp';
             end
         end
+
+        %% Function to move the robot
+        function MoveRobot(self,qTrajectory)
+            for i=1:numrows(qTrajectory)
+                self.model.animate(qTrajectory(i,:));
+                drawnow()
+            end
+        end
+
+        %% Test the movement of robot
+        function TestMovement(self)
+            qTest = rand(1,5);
+            qTrajectory = jtraj(self.defaultJoint,qTest,100);
+            self.MoveRobot(qTrajectory);
+        end
     end
 end
+
+
